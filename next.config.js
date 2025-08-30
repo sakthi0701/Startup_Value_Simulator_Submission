3ca89d2a9f9688c9b72a5030b1b47efb2e449662
sakthi0701 <154730099+sakthi0701@users.noreply.github.com>
2025-08-30 19:20:52 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 module.exports = nextConfig;
