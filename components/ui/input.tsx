762f5c11823921bb87a8695b456493ee49f634fb
sakthi0701 <154730099+sakthi0701@users.noreply.github.com>
2025-08-30 19:59:54 +0530

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,12 +1,32 @@
 import * as React from "react";
-
 import { cn } from "@/lib/utils";
+import { Label } from "./label";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, label, ...props }, ref) => {
+    const id = props.id || React.useId();
+    if (label) {
+      return (
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor={id}>{label}</Label>
+          <input
+            type={type}
+            id={id}
+            className={cn(
+              "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+              className
+            )}
+            ref={ref}
+            {...props}
+          />
+        </div>
+      );
+    }
     return (
       <input
         type={type}
